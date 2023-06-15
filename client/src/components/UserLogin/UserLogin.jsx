93704a425545f13deb653730f29967a8d8d6ec98
fayiztv <tvfayiztv@gmail.com>
2023-06-15 23:03:13 +0530

    user login ui created

---
+++ client/src/components/UserLogin/UserLogin.jsx
@@ -0,0 +1,115 @@
+
+import React, { useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { CircularProgress, TextField } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import "../UserSignup/signup.css"
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners'
+import loginImage from '/src/assets/images/login2.png'
+
+function UserLogin() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errMessage, setErrMessage] = useState("")
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState({
+        submit: false
+    })
+
+
+    const validForm = () => {
+        if (password.trim() === "" || email.trim() === "") {
+            return false
+        }
+        return true
+    }
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+        setLoading({...loading, submit:true})
+        const {data}= await axios.post("/user/auth/login", {email, password});
+        if(data.err){
+            setErrMessage(data.message)
+        }else{
+            dispatch({ type: "refresh" })
+        }
+        setLoading({...loading, submit:false})
+    }
+
+    return (
+        <div className="login-main">
+            <Row>
+                <nav className='login-nav'>
+                    <Container>
+                        <Row>
+                            <h3>onmyWay</h3>
+                        </Row>
+                    </Container>
+                </nav>
+            </Row>
+            <Row>
+                <div className="login-container">
+                    <Row>
+
+                        <Col md={6}>
+                            <div className="login-sec bg">
+                                <img src={loginImage} alt="" />
+                            </div>
+                        </Col>
+                        <Col md={6}>
+                            <div className="login-sec">
+                                <form className="login-box" onSubmit={handleSubmit}>
+                                    <div className="login-row head">
+                                        <h3>Login</h3>
+                                    </div>
+                                    <div className="login-row w-100">
+                                                 <TextField
+                                                  id="filled-textarea"
+                                                  label="Email"
+                                                  variant="standard"
+                                                  type="email"
+                                                  fullWidth
+                                                  value={email} onChange={(e)=>setEmail(e.target.value)}
+                                                  />
+                                            </div>
+                                            <div className="login-row">
+                                                 <TextField
+                                                  id="filled-textarea"
+                                                  label="password"
+                                                  variant="standard"
+                                                  type="password"
+                                                  fullWidth
+                                                  value={password} onChange={(e)=>setPassword(e.target.value)}
+                                                  />
+                                            </div>
+                                            {
+                                                errMessage &&
+                                                <div className="login-row" style={{ justifyContent: "flex-start" }}>
+                                                    <p className='text-danger'>{errMessage}</p>
+                                                </div>
+                                            }
+                                    <div className="login-row d-flex mt-1 justify-content-start">
+                                        <Link to="/forgot">Forgot Password</Link>
+                                    </div>
+                                    <div className="login-row">
+                                        <button type='submit' className='w-100' disabled={!validForm()}>
+                                            login
+                                            <ClipLoader size={20} color="white" loading={loading.submit} />
+                                            </button>
+                                    </div>
+                                    <div className="login-row mt-3">
+                                        <Link to="/sign-up">Don't Have an Account? Signin</Link>
+                                    </div>
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Row>
+        </div>
+    )
+
+}
+
+export default UserLogin
---
+++ client/src/components/VerifyOtp/VerifyOtp.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { ClipLoader } from "react-spinners";
+import axios from "axios";
+import "../UserSignup/signup.css";
+import { Col, Row } from "react-bootstrap";
+import otpImage from "/src/assets/images/otp.png";
+import { TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+function VerifyOtp(props) {
+  const [errMessage, setErrMessage] = useState("");
+  const [otp, setOtp] = useState("");
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState({
+    submit: false,
+  });
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading({ ...loading, submit: true });
+    let { data } = await axios.post("/user/auth/register/verify", {
+      otp,
+      ...props.data,
+    });
+    if (data.err) {
+      setErrMessage(data.message);
+    } else {
+      dispatch({ type: "refresh" });
+      navigate("/login");
+    }
+    setLoading({ ...loading, submit: false });
+  }
+  return (
+    <Row>
+      <div className="login-container">
+        <Row>
+          <Col md={6}>
+            <div className="login-sec bg">
+              <img src={otpImage} alt="" />
+            </div>
+          </Col>
+          <Col md={6}>
+            <form className="login-sec sec-2" onSubmit={handleSubmit}>
+              <div className="login-box">
+                <div className="login-row head">
+                  <h3>Verify Email</h3>
+                </div>
+                <div className="login-row head">
+                  <b>Enter the OTP</b>
+                </div>
+                <div className="login-row w-100 mt-3">
+                  <TextField
+                    id="filled-textarea"
+                    label="Otp"
+                    variant="standard"
+                    type="text"
+                    fullWidth
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                  />
+                </div>
+
+                {errMessage && (
+                  <div
+                    className="login-row"
+                    style={{ justifyContent: "flex-start" }}
+                  >
+                    <p className="text-danger">{errMessage}</p>
+                  </div>
+                )}
+                <div className="login-row">
+                  <button
+                    className="w-100"
+                    type="submit"
+                    disabled={otp.trim() == ""}
+                  >
+                    Check
+                    <ClipLoader
+                      size={20}
+                      color="white"
+                      loading={loading.submit}
+                    />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Row>
+  );
+}
+
+export default VerifyOtp;
