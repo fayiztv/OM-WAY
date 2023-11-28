5c07c0cc1ee40bc65fad17ee7ed0f022ddd821fa
fayiztv <tvfayiztv@gmail.com>
2023-11-28 11:04:10 +0530

    edited

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function dbConnect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1/onmyway');
+    await mongoose.connect(process.env.MONGOURI);
     console.log('Database connected');
   } catch (err) {
     console.error('Database error:\n', err);
